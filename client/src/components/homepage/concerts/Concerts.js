d2839ef73180a1749bb0749b3555b99854ddd173
xred13 <fabiosfendrychramos10@gmail.com>
2020-05-16 21:02:14 +0100

    fixed bugged where concerts were not being fetched when openning the react app without any further actions

--- client/src/components/homepage/concerts/Concerts.js
+++ client/src/components/homepage/concerts/Concerts.js
@@ -6,67 +6,82 @@ import "./../../../sass/homepage/concerts/Concerts.scss";
 class Concerts extends Component {
   state = {
     genre: null,
-    concerts: []
+    concerts: [],
   };
 
-  componentWillReceiveProps = nextProps => {
+  componentDidMount(){
+    this.setState({
+      genre: this.props.currentGenre
+    },
+      this.getConcertsOfGenre
+    )
+  }
 
+  componentWillReceiveProps(nextProps){
     if (nextProps.currentGenre === null) {
       return;
     }
 
     this.setState({
       genre: nextProps.currentGenre
-    });
+    }, 
+      this.getConcertsOfGenre
+    )
+  };
 
-    axios.request({
-      url: "http://localhost:8080/concerts/get-concerts-of-genre",
-      method: "POST",
+  getConcertsOfGenre = () => {
+    axios
+      .request({
+        url: "http://localhost:8080/concerts/get-concerts-of-genre",
+        method: "POST",
 
-      headers: {
-        "Content-Type": "application/json"
-      },
+        headers: {
+          "Content-Type": "application/json",
+        },
 
-      data:{
-        genre: nextProps.currentGenre
-      }
-
-    }).then((response) => {
-
-      console.log("Fetched concerts of genre successfully!")
-
-      this.setState({
-        concerts: response.data
+        data: {
+          genre: this.state.genre,
+        },
       })
+      .then((response) => {
+        console.log("Fetched concerts of genre successfully!");
 
-    }).catch((response) => {
-
-      console.log("Something went wrong!")
-
-    });
-
+        this.setState({
+          concerts: response.data,
+        });
+      })
+      .catch((response) => {
+        console.log("Something went wrong!");
+      });
   };
 
   removeConcert = (index) => {
-
-    let concertsUpdated = this.state.concerts
+    let concertsUpdated = this.state.concerts;
 
     concertsUpdated.splice(index, 1);
 
     this.setState({
-      concerts: concertsUpdated
-    })
-  }
+      concerts: concertsUpdated,
+    });
+  };
 
   render() {
     return (
       <div id="concerts">
         {this.state.concerts.length !== 0 ? (
           this.state.concerts.map((concertData, i) => (
-            <ConcertCard concertData={concertData} displayedInProfile={false} removeConcert={this.removeConcert} index={i} key={i} />
+            <ConcertCard
+              concertData={concertData}
+              displayedInProfile={false}
+              removeConcert={this.removeConcert}
+              index={i}
+              key={i}
+            />
           ))
         ) : (
-          <div id="no-concerts-warning">Sorry, no concerts for this given genre!</div>
+          <div id="no-concerts-warning">
+            Sorry, no concerts for this given genre!
+          </div>
         )}
       </div>
     );
