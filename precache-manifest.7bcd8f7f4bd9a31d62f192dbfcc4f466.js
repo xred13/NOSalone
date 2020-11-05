7d47dc323dd2ccb6a46ea3b6839136e9d7a438c4
xred13 <fabiosfendrychramos10@gmail.com>
2020-11-05 12:16:40 +0000

    Updates

---
+++ precache-manifest.7bcd8f7f4bd9a31d62f192dbfcc4f466.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f20a808db2bc3d4860cea353124593",
+    "url": "/NOSalone/index.html"
+  },
+  {
+    "revision": "6298a245fbc8cf204566",
+    "url": "/NOSalone/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "553c60368f639628fa6d",
+    "url": "/NOSalone/static/css/main.59eeacb6.chunk.css"
+  },
+  {
+    "revision": "6298a245fbc8cf204566",
+    "url": "/NOSalone/static/js/2.9a040ce4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/NOSalone/static/js/2.9a040ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553c60368f639628fa6d",
+    "url": "/NOSalone/static/js/main.50c780a3.chunk.js"
+  },
+  {
+    "revision": "54efb180982818d32717",
+    "url": "/NOSalone/static/js/runtime-main.389f8db9.js"
+  },
+  {
+    "revision": "5dc8cb1380fc554f09e2e65faf93af75",
+    "url": "/NOSalone/static/media/404.5dc8cb13.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/NOSalone/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
